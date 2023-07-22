c6a681751887d941ab60d075ad7d6641ec4a6f8a
LM <lucas.mosig@rwth-aachen.de>
2023-07-22 02:37:02 +0200

    Added animations for two cases. fixed some flickering

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ CRGB leds[NUM_LEDS];
 #define CYCLE_TIME 1
 int counter = 0;
 int last_position = 100;
+int compare_position = 100;
 
 void setup() {
   // put your setup code here, to run once:
@@ -41,13 +42,15 @@ int angle() {
 }
 
 bool stationary(int latest_position){
-  //function that checks whether the current position is the same as it was 100 ms ago
+  //function that checks whether the current position is the same as it was 200-400 ms ago
   counter = counter + 1;
-  if(counter > 100/CYCLE_TIME){
+  if(counter > 200/CYCLE_TIME){
     counter = 0;
+    compare_position = last_position;
     last_position = latest_position;
   }
-  if(latest_position == last_position){
+  //check if last two recorded positions are identical to current position. If only comparing to one it can lead to issues with very fast/slow spin resonating positions
+  if(latest_position == compare_position && latest_position == last_position){
    return true;
   } 
   else{
@@ -67,11 +70,15 @@ void draw_line(int head_led){
     }
 }
 
+int identify_field(int current_LED){
+  int field = current_LED / 5;
+  return field;
+}
 
-void highlight_field(int current_LED){
+void highlight_field(int current_field){
   //highlight current slice of the field (all 6 LEDs on the outer circle and both border lines)
   // calculate lower bound of slice
-  int lower_LED = current_LED / 5 *5;
+  int lower_LED = current_field *5;
   // turn on border, catch turning over at 49
   for (int i = 0; i < 6; i++){
     if(lower_LED+i >49){
@@ -87,15 +94,57 @@ void highlight_field(int current_LED){
   draw_line(lower_LED + 5);
 }
 
-void loop() {
-  //Set every LED to black
-  fill_solid 	(leds, 150, CRGB::Black);
+void play_field_animation(int current_field){
+  switch (current_field){
+    case 0:
+    // left neighbor
 
+    break;
+    case 1:
+    case 6:
+    //all
+    fill_solid 	(leds, 150, CRGB::Red);
+    break;
+    case 2:
+    //left side
+    fill_solid 	(leds, 25, CRGB::Red);
+    break;
+    case 3:
+    case 8:
+    //you drink
+    
+    break;
+    case 4:
+    case 9:
+    //drink and spin again
+    
+    break;
+    case 5:
+    //simon drinks
+
+    break;
+    case 7:
+    //right side
+    break;
+
+  }
+}
+
+void loop() {
   //find current position
   int current_LED = angle(); 
 
+    //Set every LED to black
+  if(not stationary(current_LED)){
+  fill_solid 	(leds, 150, CRGB::Black);
+  }
+
   if(stationary(current_LED)){
-    highlight_field(current_LED);
+    int active_field = identify_field(current_LED);
+    highlight_field(active_field);
+    FastLED.show();
+    delay(300);
+    play_field_animation(active_field);
   }
   else{
     //set current LED
@@ -108,7 +157,6 @@ void loop() {
     }
   }
   
-
   //show canvas
   FastLED.show();
   delay(CYCLE_TIME);
